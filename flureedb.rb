6d108e5270f9c23dbb10700bc8089946a8c50449
Lillia P <lpodlog@flur.ee>
2019-05-31 16:42:30 -0400

    Bump version, set to version 0.9.5-preview 3

--- flureedb.rb
+++ flureedb.rb
@@ -1,7 +1,7 @@
 class Flureedb < Formula
   desc "Graph database with a blockchain backbone"
   homepage "https://www.flur.ee"
-  url "https://fluree-releases-public.s3.amazonaws.com/fluree-latest.zip", :tag => 'latest'
+  url "https://fluree-releases-public.s3.amazonaws.com/fluree-0.9.5-PREVIEW3.zip", :tag => 'v0.9.5-3'
   sha256 "240a285b75a41f5f4bdefc285d666b77033ee2bcb95b6ce74cbf9a3f4b9d80e4"
   version "1.0.1"
 
