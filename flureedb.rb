c062d9c88fab333e956aff0c3942250191febc28
Lillia P <lpodlog@flur.ee>
2019-05-31 16:44:59 -0400

    Fix sha-256

--- flureedb.rb
+++ flureedb.rb
@@ -2,7 +2,7 @@ class Flureedb < Formula
   desc "Graph database with a blockchain backbone"
   homepage "https://www.flur.ee"
   url "https://fluree-releases-public.s3.amazonaws.com/fluree-0.9.5-PREVIEW3.zip", :tag => 'v0.9.5-3'
-  sha256 "240a285b75a41f5f4bdefc285d666b77033ee2bcb95b6ce74cbf9a3f4b9d80e4"
+  sha256 "2837836b651a76105c428e8b450d0d4c86bce2fcda434435b663bbbdf149c9c8"
   version "1.0.1"
 
   def install
